33f5a6f9e518bc4b1ab5abdc44edb870433631db
Jason Tatem <jattdb@gmail.com>
2017-12-17 10:06:39 -0500

    Working on section 2

--- simple3d.py
+++ simple3d.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Point(object):
     def __init__(self, x, y, z):
         self.value = (x, y, z)
@@ -40,6 +43,36 @@ class Vector(object):
         self.value = new_vector.value
         return self
 
+    def rotate_xy(self, degrees):
+        theta = math.radians(degrees)
+        matrix = list()
+        matrix.append([math.cos(theta), -1 * math.sin(theta), 0])
+        matrix.append([math.sin(theta), math.cos(theta), 0])
+        matrix.append([0, 0, 1])
+        new_vector = linear_transform(self, matrix)
+        self.value = new_vector.value
+        return self
+
+    def rotate_xz(self, degrees):
+        theta = math.radians(degrees)
+        matrix = list()
+        matrix.append([math.cos(theta), 0, math.sin(theta)])
+        matrix.append([0, 1, 0])
+        matrix.append([-1 * math.sin(theta), 0, math.cos(theta)])
+        new_vector = linear_transform(self, matrix)
+        self.value = new_vector.value
+        return self
+
+    def rotate_yz(self, degrees):
+        theta = math.radians(degrees)
+        matrix = list()
+        matrix.append([1, 0, 0])
+        matrix.append([0, math.cos(theta), -1 * math.sin(theta)])
+        matrix.append([0, math.sin(theta), math.cos(theta)])
+        new_vector = linear_transform(self, matrix)
+        self.value = new_vector.value
+        return self
+
 
 def add_vectors(vector1, vector2):
     result = []
@@ -53,6 +86,13 @@ def subtract_vectors(vector1, vector2):
     for i in range(0, 3):
         result.append(vector1.value[i] - vector2.value[i])
     return Vector(*result)
+
+
+def linear_transform(vector, transform_matrix):
+    x = transform_matrix[0][0] * vector.value[0] + transform_matrix[0][1] * vector.value[1] + transform_matrix[0][2] * vector.value[2]
+    y = transform_matrix[1][0] * vector.value[0] + transform_matrix[1][1] * vector.value[1] + transform_matrix[1][2] * vector.value[2]
+    z = transform_matrix[2][0] * vector.value[0] + transform_matrix[2][1] * vector.value[1] + transform_matrix[2][2] * vector.value[2]
+    return Vector(x, y, z)
 
 
 def section1_validation():
@@ -75,5 +115,5 @@ def section1_validation():
 
 
 if __name__ == '__main__':
-    section1_validation()
+    pass
 
